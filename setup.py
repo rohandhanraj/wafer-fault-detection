f8bedb42b63d9d9b33ece81e44c35ae78203f126
Rohan Dhanraj Yadav <80356823+rohandhanraj@users.noreply.github.com>
2022-01-18 11:27:25 +0530

    Initial Commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="wafer-fault-detection",
     version="0.0.1",
-    description="Machine LearningL Project",
+    description="Machine Learning Project -> Detect Fault in SENSOR WAFERS",
     author="Rohan Dhanraj Yadav", 
     packages=find_packages(),
     license="MIT"
