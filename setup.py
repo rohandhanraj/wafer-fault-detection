ab8f363739df11046b6ed8ac497dfcc376454a5d
Rohan Dhanraj Yadav <80356823+rohandhanraj@users.noreply.github.com>
2022-01-18 11:25:55 +0530

    Initial Commit

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="wafer-fault-detection",
-    version="0.0.3",
-    description="ML project",
-    author="Avnish yadav", 
+    version="0.0.1",
+    description="Machine LearningL Project",
+    author="Rohan Dhanraj Yadav", 
     packages=find_packages(),
     license="MIT"
 )
